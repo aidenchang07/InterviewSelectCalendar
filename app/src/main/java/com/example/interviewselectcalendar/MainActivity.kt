6831d13495b9fa2cf3b869ee826090d49f184e29
AidenChang <asdf62210@gmail.com>
2025-07-01 22:08:57 +0800

    使用TopAppBar

--- app/src/main/java/com/example/interviewselectcalendar/MainActivity.kt
+++ app/src/main/java/com/example/interviewselectcalendar/MainActivity.kt
@@ -6,20 +6,35 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.interviewselectcalendar.ui.theme.InterviewSelectCalendarTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             InterviewSelectCalendarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            modifier = Modifier.shadow(8.dp),
+                            title = {
+                                Text(text = "Interview Select Calendar")
+                            }
+                        )
+                    }
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
