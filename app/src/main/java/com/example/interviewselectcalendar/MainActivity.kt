d59c2334ff734e6d6432c248fcbfe1d4844c9cb6
AidenChang <asdf62210@gmail.com>
2025-07-04 23:57:45 +0800

    設定左右箭頭

--- app/src/main/java/com/example/interviewselectcalendar/MainActivity.kt
+++ app/src/main/java/com/example/interviewselectcalendar/MainActivity.kt
@@ -6,7 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -31,6 +36,24 @@ class MainActivity : ComponentActivity() {
                             modifier = Modifier.shadow(8.dp),
                             title = {
                                 Text(text = "Interview Select Calendar")
+                            },
+                            navigationIcon = {
+                                IconButton(
+                                    onClick = {
+                                        println("aiden click left")
+                                    }
+                                ) {
+                                    Icon(Icons.Default.ArrowBack, "上一週")
+                                }
+                            },
+                            actions = {
+                                IconButton(
+                                    onClick = {
+                                        println("aiden click right")
+                                    }
+                                ) {
+                                    Icon(Icons.Default.ArrowForward, "下一週")
+                                }
                             }
                         )
                     }
