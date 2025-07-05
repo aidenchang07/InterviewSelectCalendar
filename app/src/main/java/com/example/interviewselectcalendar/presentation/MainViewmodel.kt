7e885e789b10c3f53e9fe9bfb9085b84ee1193f7
AidenChang <asdf62210@gmail.com>
2025-07-05 13:05:27 +0800

    新增viewmodel

---
+++ app/src/main/java/com/example/interviewselectcalendar/presentation/MainViewmodel.kt
@@ -0,0 +1,10 @@
+package com.example.interviewselectcalendar.presentation
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by AidenChang on 2025/7/5
+ */
+class MainViewmodel : ViewModel() {
+
+}
