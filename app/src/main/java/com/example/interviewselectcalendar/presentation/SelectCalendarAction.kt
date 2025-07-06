71ab9079953995600c3fae8d6d3d6e49151b88a2
AidenChang <asdf62210@gmail.com>
2025-07-06 22:12:49 +0800

    新增上/下週的動作action

--- app/src/main/java/com/example/interviewselectcalendar/presentation/SelectCalendarAction.kt
+++ app/src/main/java/com/example/interviewselectcalendar/presentation/SelectCalendarAction.kt
@@ -6,3 +6,13 @@ package com.example.interviewselectcalendar.presentation
  * SelectCalendar的動作事件
  */
 sealed interface SelectCalendarAction
+
+/**
+ * 點擊「上一週」的動作
+ */
+sealed class OnPrev : SelectCalendarAction
+
+/**
+ * 點擊「下一週」的動作
+ */
+sealed class OnNext : SelectCalendarAction
